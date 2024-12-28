a0526099041d36313b52e2ddca0c6692ed65cf8e
MuriloAlvarez <murilo12alvarez@gmail.com.br>
2024-12-28 19:48:49 -0300

    feat: ✨  Removendo rotas manuais

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,37 +1,19 @@
 import fastify from 'fastify';
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod';
-import z from 'zod';
-import { createGoal } from '../functions/create-goal';
-import { getWeekPendingGoals } from '../functions/getWeekPendingGoals';
+import { getWeekPendingGoalsRoute } from './routes/get-week-pending-goals';
+import { createGoalCompletionsRoute } from './routes/create-goal-completion';
+import { createGoalRoute } from './routes/create-goal';
+import { getWeekSummaryRoute } from './routes/get-week-summary';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.get('/goals-pending', async () => {
-  const { pendingGoals } = await getWeekPendingGoals();
-  return { pendingGoals };
-});
-
-app.post(
-  '/goals',
-  {
-    schema: {
-      body: z.object({
-        title: z.string(),
-        desiredWeeklyFrequency: z.number().int().min(1).max(7),
-      }),
-    },
-  },
-  async (request) => {
-    const { desiredWeeklyFrequency, title } = request.body;
-    await createGoal({
-      title,
-      desiredWeeklyFrequency,
-    });
-  },
-);
+app.register(createGoalRoute);
+app.register(getWeekPendingGoalsRoute);
+app.register(createGoalCompletionsRoute);
+app.register(getWeekSummaryRoute);
 
 app
   .listen({
@@ -41,4 +23,4 @@ app
     console.log('hello world');
   });
 
-//1:43:00
+// 07:30
