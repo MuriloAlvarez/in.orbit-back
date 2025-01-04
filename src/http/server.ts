256649475abf321afe8d88e11c9cdf7cd3c881d1
MuriloAlvarez <murilo12alvarez@gmail.com.br>
2025-01-04 16:28:40 -0300

    feat: ✨ Criando função para listragem de metas concluídas e exibição de progresso e configurando fastify CORS

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,8 +4,13 @@ import { getWeekPendingGoalsRoute } from './routes/get-week-pending-goals';
 import { createGoalCompletionsRoute } from './routes/create-goal-completion';
 import { createGoalRoute } from './routes/create-goal';
 import { getWeekSummaryRoute } from './routes/get-week-summary';
+import fastifyCors from '@fastify/cors';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: '*',
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
@@ -22,5 +27,3 @@ app
   .then(() => {
     console.log('hello world');
   });
-
-// 07:30
